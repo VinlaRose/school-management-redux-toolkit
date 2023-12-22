db73f1632cedd4a0974b918ac6afc5d216c6a62f
VinlaRose <vinla.files@gmail.com>
2023-12-22 15:37:32 +0530

    github link added

--- src/components/HomeView.jsx
+++ src/components/HomeView.jsx
@@ -3,7 +3,7 @@ export const HomeView = () => {
         <div>
             <div>CMC Hospital</div>
             <div className="footer">
-  <p> <a href="https://github.com/VinlaRose/patient-management">Github</a> </p>
+  <p> <a href="https://github.com/VinlaRose/school-management-redux-toolkit">Github</a> </p>
 </div>
         </div>
     )
